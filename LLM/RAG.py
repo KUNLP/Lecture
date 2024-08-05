9a4c7489758bdf98d2d44f8225db30f6eba776ca
KUNLP <77040327+KUNLP@users.noreply.github.com>
2024-08-05 16:02:16 +0900

    Update RAG.py

--- LLM/RAG.py
+++ LLM/RAG.py
@@ -1,1 +1,114 @@
+from torch import cuda, bfloat16
+import torch
+import transformers
+from transformers import AutoTokenizer
+from langchain.llms import HuggingFacePipeline
+from langchain.document_loaders import TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.chains import RetrievalQA
+from langchain.vectorstores import Chroma
 
+model_id = ‘Qwen/Qwen2-7B-Instruct’
+
+bnb_config = transformers.BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type='nf4',
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_compute_dtype=bfloat16
+)
+
+model_config = transformers.AutoConfig.from_pretrained(
+    model_id,
+)
+
+model = transformers.AutoModelForCausalLM.from_pretrained(
+    model_id,
+    trust_remote_code=True,
+    config=model_config,
+    quantization_config=bnb_config,
+    device_map='auto',
+)
+
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+
+query_pipeline = transformers.pipeline(
+        "text-generation",
+        model=model,
+        tokenizer=tokenizer,
+        torch_dtype=torch.float16,
+        device_map="auto",)
+
+query_pipeline = transformers.pipeline(
+        "text-generation",
+        model=model,
+        tokenizer=tokenizer,
+        torch_dtype=torch.float16,
+        device_map="auto",)
+
+def test_model(tokenizer, pipeline, prompt_to_test):
+    """
+    Perform a query
+    print the result
+    Args:
+        tokenizer: the tokenizer
+        pipeline: the pipeline
+        prompt_to_test: the prompt
+    Returns
+        None
+    """
+sequences = pipeline(
+        prompt_to_test,
+        do_sample=True,
+        top_k=10,
+        num_return_sequences=1,
+        eos_token_id=tokenizer.eos_token_id,
+        max_length=200,)
+for seq in sequences:
+        print(f"Result: {seq['generated_text']}")
+
+test_model(tokenizer,
+           query_pipeline,
+           "Please explain what is the State of the Union address. Give just a definition. Keep it in 100 words.")
+
+llm = HuggingFacePipeline(pipeline=query_pipeline)
+# checking again that everything is working fine
+llm(prompt="Please explain what is the State of the Union address. Give just a 		definition. Keep it in 100 words.")
+
+loader = TextLoader(“../biden-sotu-2023-planned-official.txt",
+                    	encoding="utf8")
+documents = loader.load()
+
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, 				chunk_overlap=20)
+all_splits = text_splitter.split_documents(documents)
+
+model_name = "sentence-transformers/all-mpnet-base-v2"
+model_kwargs = {"device": "cuda"}
+
+embeddings = HuggingFaceEmbeddings(model_name=model_name, 			model_kwargs=model_kwargs)
+
+vectordb = Chroma.from_documents(documents=all_splits, 				embedding=embeddings, persist_directory="chroma_db")
+
+retriever = vectordb.as_retriever()
+
+qa = RetrievalQA.from_chain_type(
+    llm=llm, 
+    chain_type="stuff", 
+    retriever=retriever, 
+    verbose=True
+)
+
+query = "What were the main topics in the State of the Union in 2023? Summarize. Keep it under 200 words."
+
+result = qa.run(query)
+print("\nResult: ", result)
+
+query = "What is the nation economic status? Summarize. Keep it under 200 words."
+docs = vectordb.similarity_search_with_score(query)
+print(f"Query: {query}")
+print(f"Retrieved documents: {len(docs)}")
+for doc in docs:
+    doc_details = doc.to_json()['kwargs']
+    print("Text: ", doc_details['page_content'], "\n")
+    print(“Score: ", doc_details[‘metadata’][‘score’], "\n")
